808db63aabccc2a4374fc6f3a39c295505911270
ArgunovAlexander <30368617+ArgunovAlexander@users.noreply.github.com>
2017-10-01 14:31:13 +0300

    Update MiniMusicPlayer2.java
    Add drawing graphics in time with the music

--- MiniMusicPlayer2.java
+++ MiniMusicPlayer2.java
@@ -1,35 +1,50 @@
 import javax.sound.midi.*;
-public class MiniMusicPlayer2 implements ControllerEventListener {
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class MiniMusicPlayer2 {
+    
+    static JFrame f=new JFrame("My first Music Video");
+    static MyDrawPanel ml;
+    
     public static void main(String [] args) {
         MiniMusicPlayer2 mini=new MiniMusicPlayer2();
         mini.go();
-    }
-  public void go() {
-    try {
+    }//close main()
+    
+    public void setUpGui() {
+        ml=new MyDrawPanel();
+        f.setContentPane(ml);
+        f.setBounds(30,30,300,300);
+        f.setVisible(true);
+    }//close SetUpGui()
+    
+    public void go() {
+        setUpGui();
+        
+        try {
+        
         Sequencer sequencer=MidiSystem.getSequencer();
         sequencer.open();
-        
-        int[] eventsIWant={127};
-        sequencer.addControllerEventListener(this, eventsIWant);
-        
+        sequencer.addControllerEventListener(ml, new int[] {127});
         Sequence seq=new Sequence(Sequence.PPQ, 4);
         Track track=seq.createTrack();
         
-        for (int i=5; i<60;i+=4) {
-            track.add(makeEvent(144,1,i,100,i));
+            int r=0;        
+            for (int i=5; i<60;i+=4) {
+            
+            r=(int) ((Math.random()*50)+1);    
+            track.add(makeEvent(144,1,r,100,i));
             track.add(makeEvent(176,1,127,0,i));
-            track.add(makeEvent(128,1,i,100,i+2));
+            track.add(makeEvent(128,1,r,100,i+2));
         }//end loop
         
         sequencer.setSequence(seq);
-        sequencer.setTempoInBPM(220);
+        sequencer.setTempoInBPM(120);
         sequencer.start();
      }catch (Exception ex) {ex.printStackTrace();}
   }//close go()
-
-public void controlChange(ShortMessage event) {
-    System.out.println("la");
-}//close controlChange
 
 public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
     MidiEvent event=null;
@@ -40,4 +55,31 @@ public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
     } catch (Exception e) {}
    return event;
    }//close makeEvent
+   
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
+        boolean msg=false;
+        public void controlChange(ShortMessage event) {
+            msg=true;
+            repaint();
+        }//close controlChange
+        
+        public void paintComponent(Graphics g) {
+          if (msg) {
+            Graphics2D g2=(Graphics2D) g;
+            int r=(int) (Math.random()*250);
+            int gr=(int) (Math.random()*250);
+            int b=(int) (Math.random()*250);
+              
+            g.setColor(new Color(r,gr,b));
+              
+            int ht=(int) ((Math.random()*120)+10);
+            int width=(int) ((Math.random()*120)+10);
+            int x=(int) ((Math.random()*40)+10);
+            int y=(int) ((Math.random()*40)+10);
+            g.fillRect(x,y,ht,width);
+            msg=false;
+          
+          }//close if
+        }//close paintComponent
+    }//close inner Class MyDrawPanel
 }
